d290a1218d0491db148e08f55da7f90bcc15fcf7
Никита Лётыч <Ljotych@gmail.com>
2023-05-24 11:56:27 +0600

    Добавлены модели, вью и сериализаторы для названий, жанров и категорий

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(verbose_name="Название категории", max_length=200)
+    slug = models.SlugField(verbose_name="Слаг категории", unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class Genres(models.Model):
+    name = models.CharField(verbose_name="Название жанра", max_length=200)
+    slug = models.SlugField(verbose_name="Слаг жанра", unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField()
+    description = models.TextField(blank=True)
+    genre = models.ForeignKey(Genres, on_delete=models.SET_DEFAULT, default="Без жанра")
+    category = models.OneToOneField(
+        Categories, on_delete=models.SET_DEFAULT, default="Без категории"
+    )
+
+    def __str__(self):
+        return self.name
