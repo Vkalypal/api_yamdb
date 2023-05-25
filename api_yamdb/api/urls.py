36328ea8a5089852e9fbcbab2847ab6dbb81f824
Loginov Mikhail <loginov.mike@gmail.com>
2023-05-25 06:02:01 +0600

    feat(models)!: все модели стали в одном числе, необходимо применить миграции

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,13 +1,30 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from .views import (CategoriesViewSet, GenresViewSet, SignUpViewSet,
-                    TitleViewSet)
+from .views import (
+    CategoryViewSet,
+    CommentViewSet,
+    GenreViewSet,
+    ReviewViewSet,
+    SignUpViewSet,
+    TitleViewSet,
+)
+
 
 router_v1 = routers.DefaultRouter()
-router_v1.register(r"titles", TitleViewSet, basename="titles")
-router_v1.register(r"categories", CategoriesViewSet, basename="categories")
-router_v1.register(r"genres", GenresViewSet, basename="genres")
+router_v1.register(r"titles", TitleViewSet, basename="title")
+router_v1.register(r"categories", CategoryViewSet, basename="category")
+router_v1.register(r"genres", GenreViewSet, basename="genre")
+router_v1.register(
+    r"titles/(?P<title_id>\d+)/reviews",
+    ReviewViewSet,
+    basename="review",
+)
+router_v1.register(
+    r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
+    CommentViewSet,
+    basename="comment",
+)
 
 
 urlpatterns = [
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,11 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 from reviews.validators import NotLaterThisYearValidator
+
+
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -52,6 +57,7 @@ class Title(models.Model):
         Category,
         verbose_name="Категория",
         related_name="titles",
+        null=True,
         on_delete=models.SET_NULL,
     )
 
@@ -80,3 +86,78 @@ class GenreTitle(models.Model):
 
     def __str__(self):
         return f"{self.title} - {self.genre}"
+
+
+class Review(models.Model):
+    title = models.ForeignKey(
+        Title,
+        verbose_name="Произведение",
+        related_name="reviews",
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField(verbose_name="Отзыв")
+    author = models.ForeignKey(
+        User,
+        verbose_name="Автор отзыва",
+        related_name="reviews",
+        on_delete=models.CASCADE,
+    )
+    score = models.PositiveSmallIntegerField(
+        verbose_name="Оценка",
+        validators=(
+            MinValueValidator(1, "Оценка не может быть менее 1"),
+            MaxValueValidator(10, "Оценка не может быть более 10"),
+        ),
+    )
+    pub_date = models.DateTimeField(
+        verbose_name="Дата публикации",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+        ordering = ["title"]
+        constraints = [
+            models.UniqueConstraint(
+                name="unique_title_author",
+                fields=["title", "author"],
+            ),
+            models.CheckConstraint(
+                check=models.Q(score__gte=1, score__lte=10),
+                name='check_score_range',
+            ),
+        ]
+
+    def __str__(self):
+        return self.text[:40] + "..."
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        verbose_name="Коментарий",
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField(
+        verbose_name="Текст коментария",
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name="Автор коментария",
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+    pub_date = models.DateTimeField(
+        verbose_name="Дата публикации комментария",
+        auto_now_add=True,
+    )
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ["review"]
+
+    def __str__(self):
+        return self.text[:40] + "..."
