9724c53a9b9410fdb9bfe0d088f0d8ebe39c67e6
Loginov Mikhail <loginov.mike@gmail.com>
2023-05-27 11:30:50 +0600

    fix(permission): добавлена корректная проверка

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,14 +2,6 @@ from rest_framework import permissions
 
 
 class IsAuthorAdminModeratorOrReadOnly(permissions.BasePermission):
-    # def has_object_permission(self, request, view, obj):
-    # TODO: дописать, а пока пускай всегда будет True
-    # return (
-    #     request.method in permissions.SAFE_METHODS
-    #     or obj.author == request.user
-    #     or "Админ"
-    #     or "Модератор"
-    # )
     def has_permission(self, request, view):
         return (
             request.method in permissions.SAFE_METHODS
